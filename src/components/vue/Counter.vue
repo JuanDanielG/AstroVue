7f69e45b38f4c8316d3a1ca60d4592beb73a83b1
JuanDanielG <jdgaleanon@ufpso.edu.co>
2024-05-03 17:01:03 -0500

    first commit

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,18 @@
+<!--el script setup sirve para ahorrar trabajo ya que vue interpreta el codigo -->
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(5);
+
+function add(){
+	counter.value++;
+	console.log(counter);
+	
+}
+</script>
+
+<template>
+	<div @click="add" class="font-semibold text-white select-none hover:cursor-pointer bg-blue-600 rounded py-1 px-2 max-w-44 text-center">
+		Número de clicks: {{ counter }}
+		</div>
+</template>
